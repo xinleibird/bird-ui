bbea7caeec9079dbdaaf8b97a50c2ca151e6fbba
xinleibird <xinleibird@gmail.com>
2020-05-25 01:23:11 +0800

    update package publish test

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,8 @@ import * as Icon from './Icon';
 import Menu from './Menu';
 import Transition from './Transition';
 
+import '../styles';
+
 export const prefix = 'bird-ui';
 
 export default { Alert, Button, Icon, Menu, Transition };
